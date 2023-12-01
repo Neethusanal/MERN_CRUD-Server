92b638801a1cc682cd293e1a90373f60b82ce3eb
Neethusanal <neethukumarkk@gmail.com>
2023-12-01 17:32:36 +0530

    api

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const usercontroller=require('../Controller/UserController')
 
 router.get('/getusers',usercontroller.getallUsers)
+//router.post('/adduser',usercontroller.addUser)
+//router.put('/edituser,usercontroller.editUser)
+//router.delete('/deleteuser,usercontroller.deleteUser)
 
 
 
